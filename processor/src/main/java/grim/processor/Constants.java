2682af2c0e6874c7d375e979cf1b16734bc120a3
Peter Donald <peter@realityforge.org>
2019-10-12 15:14:19 +1100

    Add constants for all annotations defined

---
+++ processor/src/main/java/grim/processor/Constants.java
@@ -0,0 +1,14 @@
+package grim.processor;
+
+final class Constants
+{
+  static final String OMIT_CLINIT_CLASSNAME = "grim.annotations.OmitClinit";
+  static final String OMIT_PATTERN_CLASSNAME = "grim.annotations.OmitPattern";
+  static final String OMIT_PATTERNS_CLASSNAME = "grim.annotations.OmitPatterns";
+  static final String OMIT_SYMBOL_CLASSNAME = "grim.annotations.OmitSymbol";
+  static final String OMIT_TYPE_CLASSNAME = "grim.annotations.OmitType";
+
+  private Constants()
+  {
+  }
+}
