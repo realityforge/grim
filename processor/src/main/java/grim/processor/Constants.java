2b9007262a072deadf809a8100a5347ef255d831
Peter Donald <peter@realityforge.org>
2019-10-13 14:20:01 +1100

    Allow symbols to be omitted in different contexts

--- processor/src/main/java/grim/processor/Constants.java
+++ processor/src/main/java/grim/processor/Constants.java
@@ -6,6 +6,7 @@ final class Constants
   static final String OMIT_PATTERN_CLASSNAME = "grim.annotations.OmitPattern";
   static final String OMIT_PATTERNS_CLASSNAME = "grim.annotations.OmitPatterns";
   static final String OMIT_SYMBOL_CLASSNAME = "grim.annotations.OmitSymbol";
+  static final String OMIT_SYMBOLS_CLASSNAME = "grim.annotations.OmitSymbols";
   static final String OMIT_TYPE_CLASSNAME = "grim.annotations.OmitType";
 
   private Constants()
