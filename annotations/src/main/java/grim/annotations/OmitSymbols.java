c04cdc50dc8ba9ee4032dc634a25663542a48f34
Peter Donald <peter@realityforge.org>
2019-10-13 14:44:31 +1100

    Ensure annotation is documented

--- annotations/src/main/java/grim/annotations/OmitSymbols.java
+++ annotations/src/main/java/grim/annotations/OmitSymbols.java
@@ -1,11 +1,13 @@
 package grim.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 /**
  * Annotation containing rules under which symbol is to be omitted from the generated javascript.
  */
+@Documented
 @Target( { ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR } )
 public @interface OmitSymbols
 {
