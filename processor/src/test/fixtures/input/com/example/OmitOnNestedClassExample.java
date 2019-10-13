9d420c6d41d263539503aeba39c5dce7fb3e9180
Peter Donald <peter@realityforge.org>
2019-10-13 15:10:55 +1100

    Add another example

---
+++ processor/src/test/fixtures/input/com/example/OmitOnNestedClassExample.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import grim.annotations.OmitType;
+
+public class OmitOnNestedClassExample
+{
+  @OmitType( when = "arez.environment==production" )
+  static class Foo
+  {
+  }
+}
